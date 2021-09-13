0f9497a82a315de6b8265b6276c95bee1a3e0ca1
AJ <aj@smartnvm.com>
2021-09-13 23:47:04 +0000

    clean up

--- assertEqual.js
+++ assertEqual.js
@@ -3,13 +3,10 @@ const happyFace = String.fromCodePoint(128516);
 const sickFace = String.fromCodePoint(129314);
 
 // FUNCTION IMPLEMENTATION
-//const errMsg = '!==';
 const assertEqual = function(actual, expected) {
-  //only outputs if assertion test fails
   (actual === expected) ?
     console.log(happyFace + ` Assertion Passed: ${actual} === ${expected}`) :
-    console.log(sickFace + `Assertion Failed! ${actual} !== ${expected}`);
-
+    console.log(sickFace + ` Assertion Failed! ${actual} !== ${expected}`);
 };
 
 // TEST CODE
