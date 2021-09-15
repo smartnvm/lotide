cd6961ce0db84491eb6118013aee0257915844a9
AJ <aj@smartnvm.com>
2021-09-15 09:44:37 +0000

    re-write to use eqArray function

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -2,29 +2,31 @@
 const happyFace = String.fromCodePoint(128516);
 const sickFace = String.fromCodePoint(129314);
 
-const assertArraysEqual = function(arr1, arr2) {
+const eqArrays = function (arr1, arr2) {
 
   if (arr1.length !== arr2.length) {
-    
-    console.log(sickFace + ` FAIL: Arrays are not equal`);
+
     return false
   }
 
   for (let i = 0; i < arr1.length; i++) {
     if (arr1[i] !== arr2[i]) {
-      console.log(sickFace + ` FAIL: Arrays are not equal`);
-      //console.log(sickFace + ` FAIL: ${arr1} !== ${arr2}`);
       return false;
     }
 
   }
 
-  console.log(happyFace + ` PASS: Arrays are identical`) ;
 
   return true;
 };
 
-assertArraysEqual([1, 5], [1, 5]);
+const assertArraysEqual = function (arr1, arr2) {
+
+  eqArrays(arr1, arr2) ? console.log(happyFace + ` PASS: Arrays are identical`) :
+    console.log(sickFace + ` FAIL: Arrays are not equal`);
+}
+
+assertArraysEqual([1, 5], [1]);
 
 console.log('--------');
 assertArraysEqual([1, 2, 3], [1, 2, 3]); // => true
