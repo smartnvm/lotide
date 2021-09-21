56b19cc7e5262f6140a5fbf7b9da0253f0de8442
AJ <aj@smartnvm.com>
2021-09-21 20:11:19 +0000

    refactor tail with shift() method

--- tail.js
+++ tail.js
@@ -7,43 +7,13 @@ const { head } = require("./head");
 
 const tail = function (arr) {
 
-  
   if (arr === undefined) {
-    return undefined;
+    return 'Error'
   }
-  if (arr.length === 2) {
-    return arr[1];
-  } 
-  if (arr.length > 2) {
-    return arr.slice(1, arr.length - 1);
-  }
-  
-  return []
+  arr.shift()
+  return arr
+
 }
 
-
-const oldTail = function (arr) {
-  //copy array
-  let newArr = arr.filter((item, index) => {
-    return index >= 0 && index < arr.length;
-  });
-
-  //Important: initialize the array!
-  //without the line below, node throws error message
-  newArr;
-
-
-  //array.splice() modifies the array
-  (newArr.length > 2) ?
-    //if true
-    newArr = newArr.splice(1, newArr.length - 1) :
-    //if false
-    (newArr.length === 2 ? newArr = newArr[1] : newArr = []);
-
-
-  //console.log('original array: ', arr);
-  return newArr;
-
-};
 
 module.exports = { tail };
