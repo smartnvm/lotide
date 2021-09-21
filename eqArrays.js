5d771ae60b99b9c77e138af6fc3b7cc86abd6944
AJ <aj@smartnvm.com>
2021-09-21 18:43:45 +0000

    refactor to use external module

--- eqArrays.js
+++ eqArrays.js
@@ -1,29 +1,15 @@
-
-const happyFace = String.fromCodePoint(128516);
-const sickFace = String.fromCodePoint(129314);
-
-// FUNCTION IMPLEMENTATION
-const assertEqual = function(actual, expected) {
-  (actual === expected) ?
-    console.log(happyFace + ` Assertion Passed: ${actual} === ${expected}`) :
-    console.log(sickFace + ` Assertion Failed! ${actual} !== ${expected}`);
-};
-
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual("Bootcamp", "Bootcamp");
-assertEqual(1, 1);
-assertEqual(1, 5);
-assertEqual(1, '1');
+const { assertEqual } = require("./assertEqual");
 
 
-const eqArrays = function(arr1, arr2) {
+
+
+const eqArrays = function (arr1, arr2) {
 
   if (arr1.length !== arr2.length) {
     return false;
 
   }
-
+ 
   for (let i = 0; i < arr1.length; i++) {
     if (arr1[i] !== arr2[i]) {
       return false;
@@ -34,10 +20,4 @@ const eqArrays = function(arr1, arr2) {
   return true;
 };
 
-assertEqual(eqArrays([1, 5], [1, 5]),true);
-
-console.log('--------');
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]),true); // => true
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), true); // => false
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]),true); // => true
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]),true); // => false
+module.exports = { eqArrays };
