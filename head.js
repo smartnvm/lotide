10ccb765bdd5f6cf6837a8960d1d680d854eeab9
AJ <aj@smartnvm.com>
2021-09-21 18:41:00 +0000

    fixed head to return undefined for non-array parameter

--- head.js
+++ head.js
@@ -1,30 +1,13 @@
-
-const happyFace = String.fromCodePoint(128516);
-const sickFace = String.fromCodePoint(129314);
-
-// FUNCTION IMPLEMENTATION
-const assertEqual = function(actual, expected) {
-  (actual === expected) ?
-    console.log(happyFace + ` Assertion Passed: ${actual} === ${expected}`) :
-    console.log(sickFace + ` Assertion Failed! ${actual} !== ${expected}`);
-};
+const { assertEqual } = require("./assertEqual");
 
 
-const head = function(arg) {
+const head = function (arg) {
   let num = 0;
-  (arg.length > 1) ? num = arg[0] : num = arg;
+  
+  (arg.length > 1) ? num = arg[0] : num = undefined
 
   return num;
 }
 
 
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual("Bootcamp", "Bootcamp");
-assertEqual(1, 1);
-assertEqual(1, 5);
-assertEqual(1, '1');
-
-assertEqual(head([5, 6, 7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([], 10));
+module.exports = { head };
