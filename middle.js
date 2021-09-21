0263951e5959eced9ba6a47bb0b3ff847b4eff0b
AJ <aj@smartnvm.com>
2021-09-21 18:44:49 +0000

    refactor with external function

--- middle.js
+++ middle.js
@@ -1,24 +1,5 @@
 
-const happyFace = String.fromCodePoint(128516);
-const sickFace = String.fromCodePoint(129314);
-
-const eqArrays = function (arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false
-  }
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-
-const assertArraysEqual = function (arr1, arr2) {
-
-  eqArrays(arr1, arr2) ? console.log(happyFace + ` PASS: Arrays are identical`) :
-    console.log(sickFace + ` FAIL: Arrays are not equal`);
-}
+const { assertArraysEqual } = require("./assertArraysEqual")
 
 
 // 'middle function takes an array and return the middle element
@@ -38,27 +19,18 @@ const middle = function (arr) {
 
   if (!isEven) { // array with odd # of elements
     midArr.push(arr[midIndex]);
-   // console.log(arr);
+    // console.log(arr);
     return midArr;
   }
 
   //midArr.push(arr[midIndex - 1]);
   //midArr.push(arr[midIndex]);
-  
+
   //slice([start], end)  end not included
-  midArr = arr.slice(midIndex - 1, midIndex+1);
+  midArr = arr.slice(midIndex - 1, midIndex + 1);
   //console.log(midArr);
 
   return midArr;
 }
 
-console.log('-----TEST CASES--------------');
-
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
-assertArraysEqual(middle(['a', 'b', 'c', 'd', 'e']), ['c']);
-assertArraysEqual(middle(['a', 'b']), []);
-assertArraysEqual(middle(['a', 'b', 1]), []);
-assertArraysEqual(middle([1, 3, 4, 5]), [3, 4]);
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-assertArraysEqual(middle([1]), []);
-assertArraysEqual(middle([1, 2]), []);
+module.exports = { middle }
