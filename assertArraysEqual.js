261045e8a1f280ec613b18aa07b3a3373e12f727
AJ <aj@smartnvm.com>
2021-09-21 18:48:27 +0000

    refactor with eqArrays module

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,35 +1,16 @@
-
-const happyFace = String.fromCodePoint(128516);
-const sickFace = String.fromCodePoint(129314);
-
-const eqArrays = function (arr1, arr2) {
-
-  if (arr1.length !== arr2.length) {
-
-    return false
-  }
-
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-
-  }
+const { eqArrays } = require("./eqArrays");
 
 
-  return true;
-};
+
+const okEmoji = String.fromCodePoint(128516);
+const failEmoji = String.fromCodePoint(129314);
 
 const assertArraysEqual = function (arr1, arr2) {
 
-  eqArrays(arr1, arr2) ? console.log(happyFace + ` PASS: Arrays are identical`) :
-    console.log(sickFace + ` FAIL: Arrays are not equal`);
+  eqArrays(arr1, arr2) ? console.log(okEmoji + ` PASS: Arrays are identical`) :
+    console.log(failEmoji + ` FAIL: Arrays are not equal`);
 }
 
-assertArraysEqual([1, 5], [1]);
 
-console.log('--------');
-assertArraysEqual([1, 2, 3], [1, 2, 3]); // => true
-assertArraysEqual([1, 2, 3], [3, 2, 1]); // => false
-assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]); // => true
-assertArraysEqual(["1", "2", "3"], ["1", "2", 3]); // => false
+module.exports = { assertArraysEqual };
+
