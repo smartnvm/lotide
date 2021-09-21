858909ea326beb7fc8e785c930e9b9d997b1bb6f
AJ <aj@smartnvm.com>
2021-09-21 19:20:53 +0000

    fixed head for array with one element

--- head.js
+++ head.js
@@ -1,13 +1,14 @@
 const { assertEqual } = require("./assertEqual");
 
+//  array with only one element should still yield that one element as its head
+//  empty array should yield undefined as its head
 
 const head = function (arg) {
   let num = 0;
-  
-  (arg.length > 1) ? num = arg[0] : num = undefined
+
+  (arg.length >= 1) ? num = arg[0] : num = undefined
 
   return num;
 }
-
 
 module.exports = { head };
